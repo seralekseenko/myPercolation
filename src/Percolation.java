d69d44c0cf88d73c127d5543e75d0f5420345e43
seralekseenko <seralekseenko@ukr.net>
2018-10-26 00:58:21 +0300

    РеалÐОформил класс Percolation и тесты к нему

---
+++ src/Percolation.java
@@ -0,0 +1,120 @@
+public class Percolation {
+    private boolean[][] siteStatus;
+    private int openSites = 0;
+
+    private MyUF UF;
+
+
+    /**
+     * Constructor.
+     * @param n - the dimension of the grid.
+     */
+    public Percolation(int n) {
+        if (n <= 0) throw new IllegalArgumentException("Invalid input argument.");
+
+        siteStatus = new boolean[n][n];
+        UF = new MyUF(n * n + 2);
+    }
+
+    /**
+     * Opens a cell in the grid.
+     * @param row - coordinate row <= 1 && row <= n
+     * @param col - coordinate col <= 1 && col <= n
+     */
+    public void open (int row, int col) {
+        if (!isLegalCoordinate(row, col)) throw new IllegalArgumentException("Invalid input argument.");
+        if (isOpen(row, col)) return;
+
+        siteStatus[row - 1][col - 1] = true;
+        if (row == 1 || row == getSize()) connectWithVirtualSites(row, col);
+        connectWithOpenSites(row, col);
+        openSites++;
+    }
+
+    /**
+     * This method indicates the status of the cell: closed or open.
+     * @param row,col - coordinate an incoming cell
+     * @return true or false
+     */
+    public boolean isOpen(int row, int col) {
+        if (!isLegalCoordinate(row, col)) throw new IllegalArgumentException("Invalid input argument.");
+        return siteStatus[row - 1][col - 1];
+    }
+
+    /**
+     * This method checks the connection to the top virtual cell. The presence of
+     * communication with the upper virtual cell indicates the fullness of the cell.
+     * @param row,col - coordinate an incoming cell
+     * @return ????
+     */
+    public boolean isFull(int row, int col) {
+        if (!isLegalCoordinate(row, col)) throw new IllegalArgumentException("Invalid input argument.");
+        return UF.isConnected(0, getIndex(row, col));
+    }
+
+    /**
+     * This method says how many open cells are in the grid.
+     * @return ??
+     */
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    /**
+     * This method checks whether the system is leaking at this moment.
+     * @return
+     */
+    public boolean percolates() {
+        return UF.isConnected(0, getSize() * getSize() + 1);
+    }
+
+    /**
+     *This method checks the validity of coordinate values.
+     * @param row - coordinate row <= 1 && row <= n
+     * @param col - coordinate col <= 1 && col <= n
+     * @return true or false
+     */
+    private boolean isLegalCoordinate(int row, int col) {
+        return row > 0 && row <= getSize() && col > 0 && col <= getSize();
+    }
+
+    private int getSize(){
+        return siteStatus.length;
+    }
+
+    /**
+     * This method is necessary for working with UF*.
+     * @param row,col - coordinate an incoming cell
+     * @return index of incoming cell
+     */
+    private int getIndex(int row, int col) {
+        return (row - 1) * getSize() + col;
+    }
+
+    /**
+     * This method combines an incoming cell with a virtual cell.
+     * @param row,col - coordinate an incoming cell
+     */
+    private void connectWithVirtualSites(int row, int col) {
+        if (row == 1) {
+            UF.union(0, getIndex(row, col));
+        } else {
+            UF.union(getSize() * getSize() + 1, getIndex(row, col));
+        }
+    }
+
+    /**
+     * This method combines an incoming cell with neighboring cells that are also open.
+     * @param row,col - coordinate an incoming cell
+     */
+    private void connectWithOpenSites(int row, int col) {
+        if (isLegalCoordinate(row - 1, col))// upper
+            UF.union(getIndex(row - 1, col), getIndex(row, col));
+        if (isLegalCoordinate(row, col - 1))// left
+            UF.union(getIndex(row, col - 1), getIndex(row, col));
+        if (isLegalCoordinate(row, col + 1))// right
+            UF.union(getIndex(row, col + 1), getIndex(row, col));
+        if (isLegalCoordinate(row + 1, col))// bottom
+            UF.union(getIndex(row + 1, col), getIndex(row, col));
+    }
+}
---
+++ test/PercolationTest.java
@@ -0,0 +1,98 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PercolationTest {
+
+
+    @Test
+    public void Percolation() {
+        Percolation percolation = new Percolation(4);
+
+        assertFalse(percolation == null);
+        assertEquals(0, percolation.numberOfOpenSites());
+    }
+
+    @Test
+    public void testOpenInvalidCoordinate() {
+        Percolation percolation = new Percolation(4);
+
+        try {
+            percolation.open(0, 3);
+            percolation.open(-1, 3);
+            percolation.open(1, 5);
+            percolation.open(0, 0);
+        } catch (IllegalArgumentException e) {
+            //e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testOpenValidCoordinate() {
+        Percolation percolation = new Percolation(4);
+
+        percolation.open(1, 1);
+        percolation.open(2, 1);
+        percolation.open(2, 2);
+        percolation.open(2, 3);
+        percolation.open(3, 3);
+        percolation.open(3, 4);
+        percolation.open(4, 4);
+    }
+
+    @Test
+    public void isOpen() {
+        Percolation percolation = new Percolation(4);
+
+        percolation.open(1, 1);
+        percolation.open(2, 1);
+        percolation.open(2, 2);
+
+        assertTrue(percolation.isOpen(1, 1));
+        assertTrue(percolation.isOpen(2, 1));
+        assertTrue(percolation.isOpen(2, 2));
+    }
+
+    @Test
+    public void isFull() {
+        Percolation percolation = new Percolation(4);
+
+        percolation.open(1, 1);
+        percolation.open(2, 1);
+        percolation.open(2, 2);
+
+        assertTrue(percolation.isFull(1, 1));
+        assertTrue(percolation.isFull(2, 2));
+        assertFalse(percolation.isFull(3, 3));
+    }
+
+    @Test
+    public void numberOfOpenSites() {
+        Percolation percolation = new Percolation(4);
+
+        percolation.open(1, 1);
+        percolation.open(2, 1);
+        percolation.open(2, 2);
+        percolation.open(2, 3);
+        percolation.open(3, 3);
+        percolation.open(3, 4);
+        percolation.open(4, 4);
+
+        assertEquals(7, percolation.numberOfOpenSites());
+    }
+
+    @Test
+    public void percolates() {
+        Percolation percolation = new Percolation(4);
+
+        percolation.open(1, 1);
+        percolation.open(2, 1);
+        percolation.open(2, 2);
+        percolation.open(2, 3);
+        percolation.open(3, 3);
+        percolation.open(3, 4);
+        percolation.open(4, 4);
+
+        assertTrue(percolation.percolates());
+    }
+}
